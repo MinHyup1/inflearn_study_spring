dc8d1b8d1dcf30559a26730fcea2c86031c90ad9
MinHyup1 <p01073027511@gmail.com>
2022-12-22 23:05:15 +0900

    다양한 의존관계 주입방법

--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -10,6 +10,9 @@ import static org.assertj.core.api.Assertions.*;
 
 public class AutoAppConfigTest {
 
+    //자동등록빈 vs 자동등록빈 (에러발생)
+    //수동등록빈 vs 수동등록빈 (수동등록빈이 우선권을가짐, 수동빈이 자동빈을 오버라이딩 해버림.)
+//    #애매한 이쁜 코드보다 길더라도 명확한 코드가 좋다
     @Test
     void basicScan() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
